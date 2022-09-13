0d401d0954f566e9b8d4b3d48595a3c8b13f6b5c
Jonathan Ferreira <jhouus33@gmail.com>
2022-09-13 00:55:58 -0300

    [ADD] Project POO

---
+++ Fundamentos/POO/POO/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using POO;
+
+
+Classe classe01 = new Classe();
+classe01.AtributoMae = "mae";
+classe01.AtributoPai = "pai";
+classe01.AtributoFilho = "filho";
+classe01.AtributoFilha = "filha";
+
+Console.WriteLine(classe01.AtributoMae);
+
